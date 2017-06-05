edc4b49f9ed523c029145173f0a733ba56e493f8
Benoit Beraud <bberaud@octo.com>
2017-06-05 10:31:26 +0200

    Initial applications

---
+++ cli/src/main/java/info/oviles/netspeedbox/App.java
@@ -0,0 +1,13 @@
+package info.oviles.netspeedbox;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
